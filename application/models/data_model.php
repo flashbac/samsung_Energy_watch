fcc24aa23dff8a47454f617a37124f42b6cd75e8
Pete <Pete@Petes-MacBook-Pro.local>
2012-12-16 17:05:50 +0100

    getLastValue

---
+++ application/models/data_model.php
@@ -0,0 +1,31 @@
+<?php
+   class Data_model extends CI_Model {
+   	
+	 public function __construct() {
+        $this -> load -> database();
+    }
+
+    public function getAllUserPosition($userID = FALSE) {
+        if (!is_numeric($userID)) {
+            return FALSE;
+    }
+	
+	public function getLastValue($meterID){
+		
+		$query = "SELECT `Value` `TimeStamp` FROM `value` WHERE `MeterID` = $meterID ORDER BY `TimeStamp` DESC LIMIT 1";
+		
+		$DBAnswer = $this -> db -> query($query);
+        $DBAnswer = $DBAnswer -> result_array();
+		
+		if (count($DBAnswer)>0) {
+            return $DBAnswer;
+        } else {
+            return FALSE;
+        }
+ 
+		return  $DBAnswer;
+	}
+	
+	
+   }
+?>
